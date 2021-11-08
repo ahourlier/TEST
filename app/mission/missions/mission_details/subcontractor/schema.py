1b2d4fe2a78d5b2d75195f987c26434da8cb649c
Stephane Conq <stephane.conq@devoteamgcloud.com>
2021-11-08 10:10:36 +0100

    started model

---
+++ app/mission/missions/mission_details/subcontractor/schema.py
@@ -0,0 +1,14 @@
+from flask_marshmallow.sqla import SQLAlchemyAutoSchema
+from marshmallow import EXCLUDE, fields
+
+from app.common.address.schema import AddressSchema
+from app.mission.missions.mission_details.subcontractor import Subcontractor
+
+
+class SubcontractorSchema(SQLAlchemyAutoSchema):
+    address = fields.Nested(AddressSchema())
+
+    class Meta:
+        model = Subcontractor
+        include_fk = True
+        unknown = EXCLUDE
