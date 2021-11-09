7364263b1ac5a86e9e902e9ab72297d5a3e655ea
Stephane Conq <stephane.conq@devoteamgcloud.com>
2021-11-09 11:34:18 +0100

    started crud elect

---
+++ app/mission/missions/mission_details/elect/model.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, Float, select
+from sqlalchemy.orm import relationship
+
+from app import db
+from app.common.base_model import BaseMixin
+from app.common.phone_number.model import HasPhones, PhoneNumber
+from sqlalchemy.ext.hybrid import hybrid_property
+
+
+class Elect(BaseMixin, db.Model):
+
+    __tablename__ = "elect"
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    mission_details_id = Column(
+        Integer(), ForeignKey("mission_detail.id"), nullable=False
+    )
+    name = Column(String(255), nullable=True)
+    job = Column(String(255), nullable=True)
+
+    @hybrid_property
+    def phone_number(self):
+        return self.phones[0] if self.phones else None
+
+    @phone_number.expression
+    def phone_number(cls):
+        return select([PhoneNumber]).where(PhoneNumber.resource_id == cls.id).first()
