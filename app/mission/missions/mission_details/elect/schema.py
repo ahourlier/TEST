38b00ca94ebf0488a186088eb66209792d19a22a
Stephane Conq <stephane.conq@devoteamgcloud.com>
2021-11-09 11:34:18 +0100

    started crud elect

--- app/mission/missions/mission_details/elect/schema.py
+++ app/mission/missions/mission_details/elect/schema.py
@@ -1,30 +1,21 @@
 from flask_marshmallow.sqla import SQLAlchemyAutoSchema
 from marshmallow import EXCLUDE, fields
 
-from app.common.phone_number.schema import PhoneNumberSchema
 from app.mission.missions.mission_details.elect import Elect
 
 
 class ElectSchema(SQLAlchemyAutoSchema):
-    phone_number = fields.Nested(PhoneNumberSchema, allow_none=True)
 
     class Meta:
         model = Elect
         include_fk = True
         unknown = EXCLUDE
-        exclude = (
-            "phones",
-        )
 
 
 class ElectUpdateSchema(SQLAlchemyAutoSchema):
     mission_details_id = fields.Integer(dump_only=True)
-    phone_number = fields.Nested(PhoneNumberSchema, allow_none=True)
 
     class Meta:
         model = Elect
         include_fk = True
         unknown = EXCLUDE
-        exclude = (
-            "phones",
-        )
