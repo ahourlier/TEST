94298bb06e3df2ace1d538ff98acb0b3888191a6
Stephane Conq <stephane.conq@devoteamgcloud.com>
2021-11-10 09:51:40 +0100

    fixed rebase miss

--- app/mission/missions/mission_details/elect/model.py
+++ app/mission/missions/mission_details/elect/model.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, Float, select
-from sqlalchemy.orm import relationship
-=======
 from sqlalchemy import Column, Integer, String, ForeignKey, select
->>>>>>> 43fddfa11ac850eb472fbfe0675f91acb70c81ed
 
 from app import db
 from app.common.base_model import BaseMixin
@@ -11,11 +6,7 @@ from app.common.phone_number.model import HasPhones, PhoneNumber
 from sqlalchemy.ext.hybrid import hybrid_property
 
 
-<<<<<<< HEAD
-class Elect(BaseMixin, db.Model):
-=======
 class Elect(HasPhones, BaseMixin, db.Model):
->>>>>>> 43fddfa11ac850eb472fbfe0675f91acb70c81ed
 
     __tablename__ = "elect"
     id = Column(Integer(), primary_key=True, autoincrement=True)
