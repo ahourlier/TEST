fd85de84bcc9761ba52d06b859fa56fb1e84afb1
Stephane Conq <stephane.conq@devoteamgcloud.com>
2021-11-23 09:43:57 +0100

    fixed list copro

--- app/common/permissions.py
+++ app/common/permissions.py
@@ -103,6 +103,9 @@ def has_mission_permission(user):
 
 def has_copro_permission(user):
     mission_id = PermissionsUtils.get_entity_id("missionId")
+    if not mission_id:
+        # Mission_id is not provided. Only admin has access to the road.
+        return user.role == UserRole.ADMIN
     missions_service.MissionService.get_by_id(mission_id)
     permission = mission_permissions.MissionPermission.check_mission_permission(
         mission_id, user
