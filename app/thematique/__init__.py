c582d805c39e2491ab1a8208f1a650cbe61fdc4f
Stephane Conq <stephane.conq@devoteamgcloud.com>
2021-12-09 11:47:54 +0100

    started model for thematiques

---
+++ app/thematique/__init__.py
@@ -0,0 +1,2 @@
+from .model import ThematiqueEntity
+from .model import ThematiqueMission
---
+++ app/thematique/model.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+
+from app import db
+
+
+class ThematiqueEntity(db.Model):
+    """ Link between thematique in firestore and entity in sql """
+
+    __tablename__ = "thematique_entity"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    resource_type = Column(String(255), nullable=False)
+    resource_id = Column(Integer(), nullable=False)
+    document_id = Column(String(255), nullable=False)
+
+
+class ThematiqueMission(db.Model):
+    """ Autorized thematiques per mission """
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    mission_id = Column(Integer(), ForeignKey("mission.id"), nullable=False)
+    thematique_name = Column(String(255), nullable=False)
+    authorized = Column(Boolean(), nullable=False, default=True)
