eca5a77e2c3b5a5e6f97839c160fc3bbf61a39c6
Stephane Conq <stephane.conq@devoteamgcloud.com>
2022-01-19 18:02:51 +0100

    removed check of memory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-import time
-import psutil
 from flask.cli import load_dotenv
 
 from app import create_app
@@ -17,8 +15,3 @@ load_dotenv()
 app = create_app(os.getenv("FLASK_ENV") or "test")
 if __name__ == "__main__":
     app.run(debug=os.getenv("DEV_SERVER"))
-    while True:
-        time.sleep(2)
-        print(
-            f"first process memory in bytes: {psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2}"
-        )  # in bytes
