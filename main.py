47c496a1f43ad9f3203caff5ae32a0e212743e5f
Stephane Conq <stephane.conq@devoteamgcloud.com>
2022-01-19 17:56:11 +0100

    added check process in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-
+import time
+import psutil
 from flask.cli import load_dotenv
 
 from app import create_app
@@ -16,3 +17,8 @@ load_dotenv()
 app = create_app(os.getenv("FLASK_ENV") or "test")
 if __name__ == "__main__":
     app.run(debug=os.getenv("DEV_SERVER"))
+    while True:
+        time.sleep(2)
+        print(
+            f"first process memory in bytes: {psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2}"
+        )  # in bytes
