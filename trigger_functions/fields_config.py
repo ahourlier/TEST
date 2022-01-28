ffe88d04373dfc108180d70c5d8266490e6c6c49
Stephane Conq <stephane.conq@devoteamgcloud.com>
2022-01-28 14:52:43 +0100

    verical: fields not updated are not disabled + update template

--- trigger_functions/fields_config.py
+++ trigger_functions/fields_config.py
@@ -1,11 +1,11 @@
 building_fields = {
     "TRAVAUX_VOTES_AG_DECOMPOSITION_DEPENSE": {
-        "travaux_pc_ht": 0,
-        "travaux_pc_ttc": 0,
-        "frais_pc_ht": 0,
-        "frais_pc_ttc": 0,
-        "depense_subventionnable_pc_ht": 0,
-        "depense_totale_pc_ttc": 0,
+        "travaux_pc_ht": None,
+        "travaux_pc_ttc": None,
+        "frais_pc_ht": None,
+        "frais_pc_ttc": None,
+        "depense_subventionnable_pc_ht": None,
+        "depense_totale_pc_ttc": None,
     }
 }
 
