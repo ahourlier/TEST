52b1dc36eb9bcfc92335805c59e5a97b5320d33b
Stephane Conq <stephane.conq@devoteamgcloud.com>
2022-02-23 14:00:14 +0100

    authorized address to be none

--- app/person/schema.py
+++ app/person/schema.py
@@ -11,7 +11,7 @@ from app.common.phone_number.schema import PhoneNumberSchema
 class PersonSchema(SQLAlchemyAutoSchema):
     phone_number = fields.Nested(PhoneNumberSchema(), allow_none=True)
     antenna = fields.Nested(AntennaSchema(), allow_none=True, required=False)
-    address = fields.Nested(AddressSchema())
+    address = fields.Nested(AddressSchema(), allow_none=True, required=False)
 
     class Meta:
         model = Person
