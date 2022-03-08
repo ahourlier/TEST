1eac0369168253c870a9d161c2f0680c8f68da09
Augustin <augustin.hourlier@devoteamgcloud.com>
2022-03-08 16:54:35 +0100

    First version complete: still in dry run

--- simulation_calculations/update_simulation_calculations.py
+++ simulation_calculations/update_simulation_calculations.py
@@ -20,8 +20,7 @@ def handle_pb(project):
             - lister les devis
     """
     from app.project.simulations.model import SimulationSubResult
-    from app.project.accommodations import Accommodation
-    from app.project.quotes.model import QuoteAccommodation
+    from app.project.simulations.model import Simulation
 
     simulations = project.simulations
     if len(simulations) == 0:
@@ -29,12 +28,6 @@ def handle_pb(project):
 
     for s in simulations:
         if needs_calculation(s):
-            total_subventions = 0
-            total_advances = 0
-            total_work_price = 0
-            remaining_cost = 0
-            subvention_on_ttc = 0
-
             sub_results = {}
             sub_results["common_area"] = {}
             sub_results["common_area"]["total_subvention"] = 0
@@ -45,6 +38,8 @@ def handle_pb(project):
             for sf in s.simulation_funders:
                 if len(sf.funder_accommodations) > 0:
                     for fa in sf.funder_accommodations:
+                        print(f"NEW FA {fa.id}")
+                        
                         if no_missing_values(fa):
                             fa.subvention = calculate_subvention(fa)
                             if fa.is_common_area:
@@ -57,112 +52,146 @@ def handle_pb(project):
                                     sub_results[fa.accommodation_id]["total_subvention"] = 0
 
                                 sub_results[fa.accommodation_id]["total_subvention"] += (
-                                    fa.subvention if fa.subvention is not None else 0
+                                    round(fa.subvention, 2) if fa.subvention is not None else 0
                                 )
+                        elif fa.accommodation_id:
+                            if fa.accommodation_id not in sub_results:
+                                sub_results[fa.accommodation_id] = {}
+                                sub_results[fa.accommodation_id]["total_subvention"] = 0
                 else:
                     pass
 
-            
 
             # Total work price (from quotes_accommodation)
             for quote in s.quotes:
-                sub_results["common_area"]["total_work_price"] += (
-                    quote.price_incl_tax if quote.price_incl_tax else 0
-                )
+                if len(quote.accommodations) == 0:
+                    # Quote with no quotes_accommodations (old useless quote)
+                    continue
                 for qa in quote.accommodations:
+                    sub_results["common_area"]["total_work_price"] += (
+                        quote.common_price_incl_tax if quote.common_price_incl_tax else 0
+                    )
+
+                    # Specific case: accommodation_id doesn't exist in funder_accommodation parsed
+                    if qa['accommodation'].id not in sub_results:
+                        print(qa['accommodation'].id)
+                        print(sub_results)
+                        continue
+                    
                     if 'total_work_price' not in sub_results[qa['accommodation'].id]:
                         sub_results[qa['accommodation'].id]["total_work_price"] = 0
+
                     sub_results[qa['accommodation'].id]["total_work_price"] += qa['price_incl_tax']
             
-            print(sub_results)
+
+            for accommodation in sub_results:
+                # Here quotes_accommodations have been found previously
+                # And values to calculate subventions exists
+                if 'total_work_price' in sub_results[accommodation] and \
+                   sub_results[accommodation]['total_subvention'] is not None :
+                    # Build remaining cost
+                    if sub_results[accommodation]["total_subvention"] > sub_results[accommodation]["total_work_price"]:
+                        sub_results[accommodation]['remaining_cost'] = 0
+                    else:
+                        sub_results[accommodation]['remaining_cost'] = abs(
+                            round(
+                                sub_results[accommodation]["total_work_price"] - sub_results[accommodation]["total_subvention"],
+                                2
+                            )
+                        )
+                    # Build subvention_on_ttc
+                    if sub_results[accommodation]["total_work_price"] == 0:
+                        # % to 0 if work_price = 0
+                        sub_results[accommodation]["subvention_on_ttc"] = 0
+                    else:
+                        sub_results[accommodation]["subvention_on_ttc"] = round(
+                            (
+                                (
+                                    sub_results[accommodation]["total_work_price"]
+                                    - sub_results[accommodation]["remaining_cost"]
+                                )
+                                / sub_results[accommodation]["total_work_price"]
+                            )
+                            * 100,
+                            0,
+                        )
+                else:
+                    print(f"Missing data for accommodation n°{accommodation}")
             
-            # sub_results[fa.accommodation_id]['total_work_price'] = get_total_price_incl_tax(s)
-            # print(sub_results)
+            total_subventions = [res.get("total_subvention") for res in sub_results.values()]
+            total_work_prices = [res.get("total_work_price", 0) for res in sub_results.values()]
+            remaining_costs = [res.get("remaining_cost", 0) for res in sub_results.values()]
+            subventions_on_ttc = [res.get("subvention_on_ttc", 0) for res in sub_results.values()]
+            write_log_in_csv(
+                [
+                    project.id,
+                    s.name,
+                    functools.reduce(
+                        lambda a, b: a + b,
+                        total_subventions
+                    ),
+                    functools.reduce(
+                        lambda a, b: a + b,
+                        total_work_prices
+                    ),
+                    functools.reduce(
+                        lambda a, b: a + b,
+                        remaining_costs
+                    ),
+                    functools.reduce(
+                        lambda a, b: a + b,
+                        subventions_on_ttc
+                    ),
+                    "None",
+                ]
+            )
 
-            # sub_results['remaining_cost'] = sub_results['total_work_price'] - total_subventions
 
-            # for sf in s.simulation_funders:
-            #     if len(sf.funder_accommodations) > 0:
-            #         for fa in sf.funder_accommodations:
-            #             if subvention_needs_calculation(fa):
-            #                 fa.subvention = calculate_subvention(fa)
-            #             if fa.is_common_area:
-            #                 sub_results["common_area"]["total_subvention"] += (
-            #                     fa.subvention if fa.subvention is not None else 0
-            #                 )
-            #             else:
-            #                 sub_results[fa.accommodation_id]["total_subvention"] += (
-            #                     fa.subvention if fa.subvention is not None else 0
-            #                 )
-            #     else:
-            #         pass
+            # Update Global Simulation
+            simulation = Simulation.query.filter(Simulation.id == s.id).first()
+            print(f"- - - - - OLD SIMULATION : {simulation.id} - - - - ")
+            print(simulation.total_subventions)
+            print(simulation.total_work_price)
+            print(simulation.remaining_costs)
+            print(simulation.subvention_on_TTC)
+            simulation.total_subventions = sum(total_subventions)
+            simulation.total_work_price = sum(total_work_prices)
+            simulation.remaining_costs = sum(remaining_costs)
+            simulation.subvention_on_TTC = sum(subventions_on_ttc)
+            print(f"- - - - - NEW SIMULATION : {simulation.id} - - - - - - ")
+            print(simulation.total_subventions)
+            print(simulation.total_work_price)
+            print(simulation.remaining_costs)
+            print(simulation.subvention_on_TTC)
 
-            # sub_results = {}
-            # for sa in s.simulations_accommodations:
-            #     sub_results[sa.accommodation_id] = {
-            #         "price_incl_tax": 0,
-            #         "total_subvention": 0,
-            #         "remaining_cost": 0,
-            #         "subvention_on_ttc": 0,
-            #     }
-            # sub_results["common_area"] = {
-            #     "id": project.common_areas.id,
-            #     "price_incl_tax": 0,
-            #     "total_subvention": 0,
-            #     "remaining_cost": 0,
-            #     "subvention_on_ttc": 0,
-            # }
-            # for quote in s.quotes:
-            #     sub_results["common_area"]["price_incl_tax"] += (
-            #         quote.price_incl_tax if quote.price_incl_tax else 0
-            #     )
-            #     for quote_accommodation in quote.accommodations:
-            #         sub_results[quote_accommodation["accommodation"].id][
-            #             "price_incl_tax"
-            #         ] += quote_accommodation["price_incl_tax"]
-            
+            # Update each Sub Result
+            sub_results_query = SimulationSubResult.query.filter(SimulationSubResult.simulation_id == s.id)
+            for accommodation in sub_results:
+                if accommodation != "common_area":
+                    sub_results_item = sub_results_query.filter(SimulationSubResult.accommodation_id == accommodation).first()
+                    if sub_results_item:
+                        print(f"- - - - - OLD SUB ITEM : {sub_results_item.id} - - - - ")
+                        print(sub_results_item.total_subvention)
+                        print(sub_results_item.work_price)
+                        print(sub_results_item.remaining_cost)
+                        print(sub_results_item.subvention_on_TTC)
 
-            # for accommodation in sub_results:
-            #     sub_results[accommodation]["remaining_cost"] = sub_results[
-            #         accommodation
-            #     ].get("price_incl_tax", 0) - sub_results[accommodation].get(
-            #         "total_subvention", 0
-            #     )
-            #     sub_results[accommodation]["subvention_on_ttc"] = round(
-            #         (
-            #             (
-            #                 sub_results[accommodation].get("price_incl_tax", 0)
-            #                 - sub_results[accommodation]["remaining_cost"]
-            #             )
-            #             / sub_results[accommodation].get("price_incl_tax", 1)
-            #         )
-            #         * 100,
-            #         0,
-            #     ) if sub_results[accommodation].get("price_incl_tax", 0) != 0 else 0
+                        sub_results_item.total_subvention = sub_results[accommodation]['total_subvention']
+                        if "total_work_price" in sub_results[accommodation]:
+                            sub_results_item.work_price = sub_results[accommodation]['total_work_price']
+                        if "remaining_cost" in sub_results[accommodation]:
+                            sub_results_item.remaining_cost = sub_results[accommodation]['remaining_cost']
+                        if "subvention_on_ttc" in sub_results[accommodation]:
+                            sub_results_item.subvention_on_TTC = sub_results[accommodation]['subvention_on_ttc']
 
-            # write_log_in_csv(
-            #     [
-            #         project.id,
-            #         s.name,
-            #         functools.reduce(
-            #             lambda a, b: a + b,
-            #             [s.get("price_incl_tax") for s in sub_results.values()],
-            #         ),
-            #         functools.reduce(
-            #             lambda a, b: a + b,
-            #             [s.get("total_subvention") for s in sub_results.values()],
-            #         ),
-            #         functools.reduce(
-            #             lambda a, b: a + b,
-            #             [s.get("remaining_cost") for s in sub_results.values()],
-            #         ),
-            #         functools.reduce(
-            #             lambda a, b: a + b,
-            #             [s.get("subvention_on_ttc") for s in sub_results.values()],
-            #         ),
-            #         "",
-            #     ]
-            # )
+                        print(f"- - - - - NEW SUB ITEM : {sub_results_item.id} - - - - ")
+                        print(sub_results_item.total_subvention)
+                        print(sub_results_item.work_price)
+                        print(sub_results_item.remaining_cost)
+                        print(sub_results_item.subvention_on_TTC)
+
+        if not DRY_RUN:
+            db.session.commit()
 
 def handle_po_tenant_sdc(project):
     """
@@ -266,9 +295,9 @@ if __name__ == '__main__':
         # Write headers
         write_csv_headers()
         # Get all projects
-        # projects = Project.query.all()
+        projects = Project.query.all()
         # Test
-        projects = [Project.query.filter(Project.id == 1549).first()]
+        # projects = [Project.query.filter(Project.id == 2007).first()]
         # Write Body
         projects_treated, unknown = parse_projects_and_write(projects)
         print(f"Total numbers of project treated: {projects_treated}")
