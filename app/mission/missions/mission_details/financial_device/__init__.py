918fef7106fda974161acd49970bcf6744b37667
Augustin <augustin.hourlier@devoteamgcloud.com>
2022-06-08 15:39:59 +0200

    feat(Missions): Add Financial Device Table

---
+++ app/mission/missions/mission_details/financial_device/__init__.py
@@ -0,0 +1,4 @@
+from flask_restx import Namespace
+from .model import FinancialDevice
+
+api = Namespace("FinancialDevice", description="FinancialDevice namespace")
---
+++ app/mission/missions/mission_details/financial_device/schema.py
@@ -0,0 +1,22 @@
+from flask_marshmallow.sqla import SQLAlchemyAutoSchema
+from marshmallow import EXCLUDE, fields
+
+from app.mission.missions.mission_details.financial_device import FinancialDevice
+
+
+class FinancialDeviceSchema(SQLAlchemyAutoSchema):
+    mission_details_id = fields.Integer(required=True)
+
+    class Meta:
+        model = FinancialDevice
+        include_fk = True
+        unknown = EXCLUDE
+
+
+class FinancialDeviceUpdateSchema(SQLAlchemyAutoSchema):
+    mission_details_id = fields.Integer(required=False)
+
+    class Meta:
+        model = FinancialDevice
+        include_fk = True
+        unknown = EXCLUDE
